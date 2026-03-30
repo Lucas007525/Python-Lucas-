992e0f3189af233d24e8948bd75ea0a24f09ec9a
Lucas007525 <lucas.ferraz.barros@escola.pr.gov.br>
2026-03-30 11:46:42 -0300

    Add files via upload

--- forca.py
+++ forca.py
@@ -2,30 +2,46 @@ def jogar_forca():
     print('*--------------------*')
     print('BEM VINDO AO JOGO DE FORCA')
     print('*--------------------*')
-    palavra_secreta = "carambóla"
-    letras_acertadas = ["_", "_", "_", "_", "_", "_", "_", "_", "_"]
+
+    arquivo = open("PYTHON-LUCAS-MAIN/palavras.txt", "r")
+    palavra = []
+
+    for linha in palavra:
+        linha = linha.strip()
+        palavras.append(linha)
+        
+    arquivo.close
+
+    palavra_secreta = "opa"
+    letras_acertadas = []
+
+    for letras in palavra_secreta:
+        letras_acertadas.append("_")
+
     perdeu = False
     acertou = False
-
+    erros = 0 
     while(not perdeu and not acertou):
-        chute = input('Digite uma Letra: ')
+        chute = input('Digite uma Letra:')
         chute = chute.strip()
 
         #Index define a posição da letra
         index = 0
-
-        for letra in palavra_secreta:
-            if(chute.lower() == letra.lower()):
-                print(f'A letra {chute} está na posição {index}!')
-                letras_acertadas[index] = letra 
-            index = index+1
-            
-
-
-
-
+        if(chute in palavra_secreta):
+            for letra in palavra_secreta:
+                if(chute == letra):
+                    print(f'A letra {chute} está na posição {index}!')
+                    letras_acertadas[index] = letra 
+                    index = index + 1 
+        else:
+            erros = erros + 1
+            acertou = "_" not in letras_acertadas
 
 
+           
+            perdeu = erros == 6 
+        print(letras_acertadas)
+        
 
 if(__name__ == "__main__"):
     jogar_forca()
